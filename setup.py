0e96a1efefdfbfd7016c28f79d784e73ae24de56
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-07-19 06:00:06 -0400

    Changed the package documentation files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='diazoframework.plone',
       version=version,
       description="A Diazo framework implementation for Plone Defaults Themes",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       # Get more strings from
       # https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
